0b808b2856dda397c1593be82d8ae6a95364dc54
ebenKB <eakbo23@gmail.com>
2020-01-08 16:13:56 +0000

    override semantic ui menu

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -16,29 +16,28 @@ export default class AccordionStandard extends Component {
   render() {
     const { activeIndex } = this.state
     return (
-        <div className="nav-wrapper">
-          <div className="logo m-b-20">logo</div>
-          <Accordion as={Menu} vertical>
-            <Menu.Item>
-              <Accordion.Title
-                active={activeIndex === 0}
-                content='Option-1'
-                index={0}
-                onClick={this.handleClick}
-              />
-              <Accordion.Content active={activeIndex === 0} content="one"  className="menu-option"/>
-            </Menu.Item>
-            <Menu.Item>
-              <Accordion.Title
-                active={activeIndex === 1}
-                content='Colors'
-                index={1}
-                onClick={this.handleClick}
-              />
-              <Accordion.Content active={activeIndex === 1} content="sub-1"  className="menu-option"/>
-            </Menu.Item>
-          </Accordion>
-        </div>
+      <div className="nav-wrapper">
+        <Accordion as={Menu} vertical>
+          <Menu.Item>
+            <Accordion.Title
+              active={activeIndex === 0}
+              content='Option-1'
+              index={0}
+              onClick={this.handleClick}
+            />
+            <Accordion.Content active={activeIndex === 0} content="one"  className="menu-option"/>
+          </Menu.Item>
+          <Menu.Item>
+            <Accordion.Title
+              active={activeIndex === 1}
+              content='Colors'
+              index={1}
+              onClick={this.handleClick}
+            />
+            <Accordion.Content active={activeIndex === 1} content="sub-1"  className="menu-option"/>
+          </Menu.Item>
+        </Accordion>
+      </div>
     );
   }
 }
