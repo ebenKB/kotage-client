8f77e28a8bc22b7738df47cf338c9e8893ea0668
ebenKB <eakbo23@gmail.com>
2020-01-16 16:08:32 +0000

    change AppRouter to a lazy component loading

--- src/components/AppRouter/router.js
+++ src/components/AppRouter/router.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Home from '../../pages/home';
-import PageNotFound from '../../pages/_404';
-import NewRequisition from '../../components/requisitions/new/new-requisitions';
-import Requisitions from '../../components/requisitions/show/requisitions-index';
-import Newquote from '../quotes/new/new-quote';
-import RFX from '../rfx/rfx';
-import Vendors from '../forms/vendors/vendors';
 
-const router = () => {
+
+const Home = lazy(() => import  ('../../pages/home'));
+const PageNotFound = lazy(() => import('../../pages/_404'));
+const NewRequisition = lazy(() => import('../../components/requisitions/new/new-requisitions'));
+const Requisitions = lazy(() => import('../../components/requisitions/show/requisitions-index'));
+const Newquote = lazy(() => import('../quotes/new/new-quote'));
+const RFX = lazy(() => import('../rfx/rfx'));
+const Vendors = lazy(() => import('../forms/vendors/vendors'));
+
+const Router = () => {
   return (
-    <Switch>
-      <Route exact path="/" >
-        <Home/>
-      </Route>
-      <Route exact path="/requisitions" component={Requisitions}/>
-      <Route path="/requisitions/new" component={NewRequisition}/>
-      <Route path="/quotes/new" component={Newquote}/>
-      <Route path="/rfx" component={RFX}/>
-      <Route path="/vendors/new" component={Vendors}/>
-      <Route path="*">
-        <PageNotFound />
-      </Route>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/requisitions" component={Requisitions}/>
+        <Route path="/requisitions/new" component={NewRequisition}/>
+        <Route path="/quotes/new" component={Newquote}/>
+        <Route path="/rfx" component={RFX}/>
+        <Route path="/vendors/new" component={Vendors}/>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Suspense>
   );
 }
 
-export default router
+export default Router
