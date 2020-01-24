cab9e3f7bf13521f6d073b91ae4c5294a809db04
ebenKB <eakbo23@gmail.com>
2020-01-24 12:17:17 +0000

    improve floating message container

--- src/components/kt-floating-message/message.js
+++ src/components/kt-floating-message/message.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import FloatingButton from '../snippets/floating-button/floating-button';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 import './message.scss';
+import KtTextArea from '../form-fields/kt-textarea/kt-textarea';
+
 
 const Message = () => {
   const [canShowMsg, setCanShowMsg] = useState(false);
@@ -10,6 +12,10 @@ const Message = () => {
     setCanShowMsg(!canShowMsg);
   }
 
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    // handle input changes here
+  }
   return (
     <div>
       <div className={`kt-message_wrapper ${canShowMsg ? 'active':'dispose'}`}>
@@ -17,8 +23,12 @@ const Message = () => {
           Please leave a comment
         </div>
         <div className="kt-message_content">
-          this is the content of the message
-          <Button className="small green">Send</Button>
+          <div className="ui form m-b-10">
+            <KtTextArea  value="The content is here"/>
+          </div>
+          <div className="text-right">
+            <Button className="small green">Send</Button>
+          </div>
         </div>
       </div>
       <FloatingButton
