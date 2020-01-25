dd3967001e49896791c8add101dc5f039977b84b
ebenKB <eakbo23@gmail.com>
2020-01-25 07:23:55 +0000

    extend default react linting rules fix linting errors in input validator fix linting errors in vendors.jsx

--- src/components/form-fields/input-validator/input-validator.js
+++ src/components/form-fields/input-validator/input-validator.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import {ValidatorComponent} from 'react-form-validator-core';
+import { ValidatorComponent } from 'react-form-validator-core';
 import { Input } from 'semantic-ui-react';
 // import Input from '../input/input';
 import FormError from '../../snippets/error-icon/error-icon';
 
-class InputValidator extends ValidatorComponent{
+class InputValidator extends ValidatorComponent {
   render() {
-    const { errorMessages, validators, requiredError, validatorListener, instantValidate, ...rest } = this.props;
-    return (
-      <>
-        <Input
-          {...rest}
-          ref = {(r) => {this.input = r;}}
-        />
-        <>{this.getErrorText()}</>
-      </>
-    );
+  const { errorMessages, validators, requiredError, validatorListener, instantValidate, ...rest } = this.props;
+  return (
+	<>
+		<Input
+      {...rest}
+      ref={(r) => { this.input = r; }}
+    />
+		<>{this.getErrorText()}</>
+	</>
+  );
   }
-  
+
   getErrorText() {
-    const {isValid} = this.state;
-    if(isValid) {
+    const { isValid } = this.state;
+    if (isValid) {
       return null; /// there is no error
     }
 
-    return(
-      <div >
-        <FormError error={this.getErrorMessage()} classes="kt-logo__very-small"/>
-      </div>
+  return (
+	<div>
+		<FormError 
+      error={this.getErrorMessage()}
+      classes="kt-logo__very-small" 
+    />
+	</div>
     )
-   }
+  }
 }
 
 export default InputValidator;
---
+++ src/components/forms/vendors/vendors.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Form } from 'semantic-ui-react';
+import './vendors.scss';
+import MainContent from '../../kt-main-content/mainContent';
+import KtWrapper from '../../kt-wrapper/kt-wrapper';
+import Divider from '../../kt-divider/divider';
+import FormGroup from '../../form-fields/form-group/form-group';
+import Collapsible from '../../snippets/collapsible/collapsible';
+import QuestionCreator from '../../snippets/question-creator/question-creator';
+
+const Vendors = () => (
+	<MainContent
+    classes='m-t-20'
+  >
+		<KtWrapper
+      header="Invite Vendor"
+    >
+			<Divider type="thick" title="Vendor Details" classes="m-t-10" />
+			<Form>
+				<div className="kt-content__wrapper">
+					<div className="m-t-30">
+						<FormGroup
+              type="text"
+              placeholder="Enter title"
+              label="Vendor Name"
+              labelName="name"
+              center={true}
+            />
+					</div>
+					<div className="m-t-30">
+						<FormGroup
+              type="email"
+              placeholder="example@email.com"
+              label="Contact Email"
+              labelName="email"
+              center={true}
+            />
+					</div>
+				</div>
+				<div className="m-t-30">
+					<Collapsible
+            title="Documents - Request documents from vendor"
+          >
+						<div className="kt-content__wrapper">
+							<QuestionCreator
+                label="Your Question"
+              />
+						</div>
+					</Collapsible>
+				</div>
+				<div className="m-t-30">
+					<Collapsible
+            title="Documents - Request custom information from vendor"
+          >
+						<div className="kt-content__wrapper">
+							<QuestionCreator
+                label="Document name"
+              />
+						</div>
+					</Collapsible>
+				</div>
+			</Form>
+		</KtWrapper>
+	</MainContent>
+);
+
+export default Vendors;
