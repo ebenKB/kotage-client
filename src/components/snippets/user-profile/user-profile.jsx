00027256cfa629cd6ec5790a331308e93a3023ad
ebenKB <eakbo23@gmail.com>
2020-02-18 13:08:22 +0000

    resolve merge conflicts with user profile

--- src/components/snippets/user-profile/user-profile.jsx
+++ src/components/snippets/user-profile/user-profile.jsx
@@ -6,22 +6,17 @@ import './user-profile.scss';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-<<<<<<< HEAD
 const UserProfile = () => {
   const logout = () => {
     console.log('we want to logout');
   };
   return (
-=======
-const UserProfile = ({ tenant }) => (
->>>>>>> add tenant account_id to users route
 	<Dropdown
 		text="John Smith"
 		pointing="top right"
 		className="user-profile"
 	>
 		<Dropdown.Menu>
-<<<<<<< HEAD
 			<Link to="/user-profile" className="item">
 				<Dropdown.Item
 					text="Your profile"
@@ -61,61 +56,6 @@ const UserProfile = ({ tenant }) => (
 					icon="setting"
 				/>
 			</Link>
-=======
-			<Dropdown.Item
-				text={(
-					<Link to="/user-profile">
-            Your profile
-					</Link>
-				)}
-				icon="user"
-				description=""
-				className="kt-success"
-			/>
-			{tenant && (
-				<Dropdown.Item
-					icon="folder"
-					text={(
-						<Link to={`/${tenant.account_id}/users`}>
-              Users
-						</Link>
-					)}
-				/>
-			)}
-			<Dropdown.Item
-				icon="folder"
-				text={(
-					<Link to="/user/invitation">
-          Invite user
-					</Link>
-				)}
-			/>
-			<Dropdown.Item
-				icon="folder"
-				text={(
-					<Link to="/user/requisitions">
-          Your requisitions
-					</Link>
-				)}
-			/>
-			<Dropdown.Item
-				text={(
-					<Link to="/john/help">
-            Help Center
-					</Link>
-				)}
-				icon="help circle"
-				description=""
-			/>
-			<Dropdown.Item
-				text={(
-					<Link to="/user/settings">
-            Settings
-					</Link>
-				)}
-				icon="setting"
-			/>
->>>>>>> add tenant account_id to users route
 			<Dropdown.Divider />
 			<Dropdown.Item
 				icon="sign-out"
