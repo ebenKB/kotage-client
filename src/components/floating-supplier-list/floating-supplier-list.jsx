9a859ac138d98b02ae7bb460bfddcd05fdfb8374
ebenKB <eakbo23@gmail.com>
2020-02-29 05:16:51 +0000

    refactor floating supplier component improve search field search icon

--- src/components/floating-supplier-list/floating-supplier-list.jsx
+++ src/components/floating-supplier-list/floating-supplier-list.jsx
@@ -5,27 +5,29 @@ import { Checkbox, Button } from 'semantic-ui-react';
 import SearchField from '../form-fields/search-field/search-field';
 import Divider from '../kt-divider/divider';
 import SupplierListItem from '../snippets/supplier-list-item/supplier-list-item';
-import { ReactComponent as Icon } from '../../svg/cancel.svg';
+import { ReactComponent as Icon } from '../../svg/cancel-white.svg';
 
 const FloatingSupplierList = ({ loading }) => (
 	<div className="supplier-float">
-		<div className="kt-wrapper__header bold big-caption light-caption">
-			<div>Supplier Directory</div>
-			<Icon className="icon-sm small logo" />
-		</div>
-		<div className="supplier-float__content">
-			<SearchField />
-			<div className="m-t-30">
-				<Checkbox label="Select All" />
-				<Divider type="thick" />
-				<div className="m-t-20">
-					<SupplierListItem />
-					<SupplierListItem />
-					<SupplierListItem />
+		<div className="supplier-float__body">
+			<div className="kt-wrapper__header bold big-caption light-caption">
+				<div>Supplier Directory</div>
+				<Icon className="icon-sm small logo" />
+			</div>
+			<div className="supplier-float__content">
+				<SearchField />
+				<div className="m-t-30">
+					<Checkbox label="Select All" />
+					<Divider type="thick" />
+					<div className="m-t-20">
+						<SupplierListItem />
+						<SupplierListItem />
+						<SupplierListItem />
+					</div>
 				</div>
 			</div>
 		</div>
-		<div className="supplier-float__footer">
+		<div className="kt-wrapper__header supplier-float__footer">
 			<div className="supplier-float__footer-content">
 				<Button
 					type="submit"
