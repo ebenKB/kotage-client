5a5c93bae4c961991d7ad8515abdacec48b1875d
ebenKB <eakbo23@gmail.com>
2020-03-02 15:30:13 +0000

    improve rich text editor

--- src/components/rich-text-editor/rich-text-editor.jsx
+++ src/components/rich-text-editor/rich-text-editor.jsx
@@ -15,19 +15,21 @@ const RichTextEditor = () => {
     console.log('The editor has changes', htmlContent);
   };
 
-  const options = ['inline', 'list', 'textAlign', 'link', 'history', 'blockType'];
+  const options = ['inline', 'list', 'textAlign', 'link', 'blockType', 'history'];
 
   const toolbarOptions = {
     options,
     inline: { inDropdown: false },
-    list: { inDropdown: false },
+    list: {
+      inDropdown: false,
+      options: ['unordered', 'ordered'],
+    },
     textAlign: { inDropdown: false },
     link: { inDropdown: false },
     blockType: {
       inDropdown: false,
       options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
       className: 'kt-block',
-      component: undefined,
     },
   };
   return (
