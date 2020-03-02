94a1c3a4d0a4993daed5ed97d99d508dc209ddb9
ebenKB <eakbo23@gmail.com>
2020-03-02 14:21:34 +0000

    override default classes for rich text editor improve rich text editor

--- src/components/rich-text-editor/rich-text-editor.jsx
+++ src/components/rich-text-editor/rich-text-editor.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './rich-text-editor.scss';
 
 const RichTextEditor = () => {
-  const handleEditorChange = () => {
-    console.log('The editor has changes');
+  const [editorState, setEditorState] = useState();
+  const [htmlContent, setHtmlContent] = useState();
+
+  const handleEditorChange = (e) => {
+    setEditorState(() => e);
+    setHtmlContent(() => draftToHtml(convertToRaw(e.getCurrentContent())));
+    console.log('The editor has changes', htmlContent);
   };
 
   const options = ['inline', 'list', 'textAlign', 'link', 'history', 'blockType'];
@@ -25,9 +32,10 @@ const RichTextEditor = () => {
   };
   return (
 	<Editor
-		toolbarClassName=""
 		wrapperClassName="kt-toolbar-wrapper"
-		editorClassName="kt-toolbar"
+		editorClassName="kt-editor"
+		toolbarClassName="kt-toolbar"
+		editorState={editorState}
 		onEditorStateChange={handleEditorChange}
 		toolbar={toolbarOptions}
 	/>
