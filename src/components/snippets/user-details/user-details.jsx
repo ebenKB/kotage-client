c22a2b3fb3612075a0b419ab2df5c77792ef6beb
ebenKB <eakbo23@gmail.com>
2020-03-15 08:28:26 +0000

    improve user details component

--- src/components/snippets/user-details/user-details.jsx
+++ src/components/snippets/user-details/user-details.jsx
@@ -12,14 +12,17 @@ import {
 import Popup from '../popup/popup';
 import { getInitialNames } from '../../../utils/app';
 
-
+/**
+ * This component displays a user or an invitation and enforces SEPARATION OF ROLES
+ * Before making modifications to this component, please do well to
+ * understand the user flow and the roles each user can perform.
+ */
 const UserDetails = ({
-  user, setUserAsAdmin, resetPass, currentTenant, currentUser, type = 'user', deleteUser, resendInvitation, deleteInvitation,
+  user, setUserAsAdmin, resetPass, currentTenant, currentUser, type = 'user',
+  deleteUser, resendInvitation, deleteInvitation,
 }) => {
   /**
-   * this function toggles the admin status of the user
-   * When called the function makes a request to the api without notifying the user
-   * Any action performed is persisted to the database asynchronously
+   * When called, the function DELIBERATELY makes a request to the api without notifying the user
    * @param {*} value the new value to set for the use. Either true or false
    */
   const handleSetAdminStatus = (value) => {
@@ -49,9 +52,14 @@ const UserDetails = ({
     }
   };
 
-  // return the nature and type of user whose details is showing
-  const getUserStatus = () => (((type === 'user' && user.id !== currentUser.id) && (currentUser.is_admin))
+  /**
+   * check if the object we are showing is a user or an invitation
+   * We check this because, this component can be used to show both a user and an invitation
+   */
+  const isUserAdmin = () => (((type === 'user' && user.id !== currentUser.id) && (currentUser.is_admin))
     || ((type === 'invitation') && (currentUser.is_admin)));
+
+  const canDeleteUser = () => (currentUser.id !== user.id && currentUser.is_admin);
 
   return (
 	<div className="user-details__wrapper">
@@ -72,8 +80,16 @@ const UserDetails = ({
 			{currentTenant.email === user.email && (
 				<div>Account owner</div>
 			)}
+			{/* check if the current user is an admin but the user is not the owner of this account */}
+			{(currentUser.email === user.email) && currentUser.is_admin
+        && !currentTenant.email === user.email && (<div className="xsm-caption">Admin</div>
+			)}
+			{/* show this only for ordinary users */}
+			{(currentUser.email === user.email) && !currentUser.is_admin && (
+				<div className="xsm-caption">User</div>
+			)}
 			<div>
-				{getUserStatus() && (
+				{isUserAdmin() && (
 					<Fragment>
 						<Checkbox
 							className="xsm-caption"
@@ -81,15 +97,19 @@ const UserDetails = ({
 							onChange={(e, data) => handleSetAdminStatus(data.checked)}
 							checked={user.is_admin}
 						/>
-						<Button
-							className="kt-transparent block kt-danger"
-							onClick={removeUser}
-						>
-              Delete user
-						</Button>
 					</Fragment>
 				)}
+				{canDeleteUser() && (
+					<Button
+						className="kt-transparent block kt-danger"
+						onClick={removeUser}
+					>
+            Delete user
+					</Button>
+				)}
 			</div>
+			{/* check whether we are showing a user and the user is not the owner of
+       this account and also the user is not the currenlty logged in user */}
 			{ type === 'user' && currentTenant.email !== user.email && currentUser.id !== user.id && (
 				<div className="m-t-10">
 					<Popup
@@ -106,7 +126,6 @@ const UserDetails = ({
 									color="green"
 									onClick={sendResetPassInstructions}
 								>
-									{' '}
                   Send reset instructions
 								</Button>
 							</div>
