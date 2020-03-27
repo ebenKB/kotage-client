dd37e7733f0d006783e7bc929be57c99ce759f43
ebenKB <eakbo23@gmail.com>
2020-03-27 10:27:54 +0000

    improve rfp show component

--- src/components/document-list-group/document-list-group.jsx
+++ src/components/document-list-group/document-list-group.jsx
@@ -7,16 +7,18 @@ import Divider from '../kt-divider/divider';
 const DocumentListGroup = ({ documents }) => (
 	<div className="kt-doc-list__group">
 		<Divider type="thick" title="REQUESTED DOCUMENTS" />
-		<div className="kt-doc-list__heading m-t-20">
-			<div>NAME</div>
-			<div>DESCRIPTION</div>
+		<div className="kt-opaque">
+			<div className="kt-doc-list__heading">
+				<div>NAME</div>
+				<div>DESCRIPTION</div>
+			</div>
+			<Divider type="faint" title="" classes="m-t-10 m-b-10" />
+			{documents && documents.map((doc) => (
+				<DocumentListItem
+					document={doc}
+				/>
+			))}
 		</div>
-		<Divider type="faint" title="" classes="m-t-10 m-b-10" />
-		{documents && documents.map((doc) => (
-			<DocumentListItem
-				document={doc}
-			/>
-		))}
 	</div>
 );
 
--- src/components/stakeholder-group/stakeholder-group.jsx
+++ src/components/stakeholder-group/stakeholder-group.jsx
@@ -9,37 +9,41 @@ import './stakeholder.scss';
 import StakeholderItem from '../stakeholder-item/stakeholder-item';
 import StakeholderItemFetch from '../stakeholder-item-fetch/stakeholder-item-fetch';
 
-// m-t-20 m-b-10 -- remove later
 const StakeholderGroup = ({
   stakeholders, addStakeholder, shouldFetchData, removeStakeholder, mode, classes,
 }) => (
 	<div>
-		<div>
-			<div className={`sm-caption faint ${classes} form-item stakeholder-group`}>
-				<div className="faint-caption">NAME OR EMAIL</div>
-				<div className="light-caption">PERMISSIONS</div>
-				{mode !== 'readonly' && (
-					<div className="light-caption">ACTION</div>
-				)}
-			</div>
-			<Divider type="faint" title="" classes="form-item m-t-8" isNumbered={false} />
-			{mode === 'write' && stakeholders && stakeholders.map((stakeholder) => (
-				<div key={stakeholder.id}>
-					<StakeholderItem
-						stakeholder={stakeholder}
-						removeStakeholder={(id) => removeStakeholder(id)}
-						mode={mode}
-					/>
-					<Divider type="faint" title="" classes="form-item m-t-8" isNumbered={false} />
+		{(stakeholders === null || stakeholders.length === 0) && mode === 'readonly' && (
+			<p>There are no stakeholders</p>
+		)}
+		{(stakeholders && stakeholders.length > 0) && (
+			<div>
+				<div className={`sm-caption faint ${classes} form-item stakeholder-group`}>
+					<div className="faint-caption">NAME OR EMAIL</div>
+					<div className="light-caption">PERMISSIONS</div>
+					{mode !== 'readonly' && (
+						<div className="light-caption">ACTION</div>
+					)}
 				</div>
-			))}
-			{mode === 'readonly' && stakeholders && stakeholders.map((stakeholder) => (
-				<StakeholderItemFetch
-					mode={mode}
-					stakeholderObj={stakeholder}
-				/>
-			))}
-		</div>
+				<Divider type="faint" title="" classes="form-item m-t-8" isNumbered={false} />
+				{mode === 'write' && stakeholders && stakeholders.map((stakeholder) => (
+					<div key={stakeholder.id}>
+						<StakeholderItem
+							stakeholder={stakeholder}
+							removeStakeholder={(id) => removeStakeholder(id)}
+							mode={mode}
+						/>
+						<Divider type="faint" title="" classes="form-item m-t-8" isNumbered={false} />
+					</div>
+				))}
+				{mode === 'readonly' && stakeholders && stakeholders.map((stakeholder) => (
+					<StakeholderItemFetch
+						mode={mode}
+						stakeholderObj={stakeholder}
+					/>
+				))}
+			</div>
+		)}
 		{mode === 'write' && (
 			<AddStakeholder
 				shouldFetchData={shouldFetchData}
