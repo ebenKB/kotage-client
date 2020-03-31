d9f22034b4d32e0a6b63afd5fe72a52f8a9c4096
ebenKB <eakbo23@gmail.com>
2020-03-31 10:53:25 +0000

    setup message center

--- src/components/message-center/message-center.jsx
+++ src/components/message-center/message-center.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Tab } from 'semantic-ui-react';
+import KtWrapperLite from '../kt-wrapper-lite/kt-wrapper-lite';
+import MainContent from '../kt-main-content/mainContent';
+import Help from '../../utils/requisitions/new/help';
+import AddItem from '../snippets/add-item/add-item';
 
-const MessageCenter = () => (
-	<div>
-    This is the message center
-	</div>
-);
+const MessageCenter = () => {
+  const panes = [
+    { menuItem: 'Inbox', render: () => <Tab.Pane>Show all inbox message here</Tab.Pane> },
+    { menuItem: 'Sent Messages', render: () => <Tab.Pane>Show all sent messages here</Tab.Pane> },
+  ];
+  return (
+	<MainContent
+		help={Help}
+	>
+		<KtWrapperLite>
+			<div className="m-b-20">
+				<AddItem
+					title="Compose Message"
+				/>
+			</div>
+			<Tab panes={panes} />
+		</KtWrapperLite>
+	</MainContent>
+  );
+};
 
 export default MessageCenter;
