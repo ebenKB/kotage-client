fb669e671c00a070c8f24be8e8d5e00090edaa6c
ebenKB <eakbo23@gmail.com>
2020-04-10 08:18:47 +0000

    resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-// import { store, persistor } from './redux/store';
-
-import { store, persistor } from './redux/store';
 import App from './App';
 
-ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<PersistGate persistor={persistor} loading={null}>
-				<App />
-			</PersistGate>
-		</Router>
-	</Provider>,
-	document.getElementById('root'),
-);
+ReactDOM.render(<Router> <App /></Router>, document.getElementById('root'));
