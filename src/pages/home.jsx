88a5e273873e46165f552eeeef6aa4404faebbf0
ebenKB <eakbo23@gmail.com>
2020-04-13 11:24:53 +0000

    remove pdf-preview component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import PdfPreview from '../components/pdf-preview/pdf-preview';
 
 const home = () => (
 	<div>
     This is the home page
-		<PdfPreview />
 	</div>
 );
 
