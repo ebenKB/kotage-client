4252343a92342ac8a4b678c41eb960ee0d39b490
ebenKB <eakbo23@gmail.com>
2020-06-02 15:56:59 +0000

    improve supplier bid dashboard

--- src/components/supplier/supplier-rfx-dashboard/supplier-rfx-dashboard.jsx
+++ src/components/supplier/supplier-rfx-dashboard/supplier-rfx-dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, Divider } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import MainContent from '../../kt-main-content/mainContent';
 import Help from '../../../utils/requisitions/new/help';
 import KtWrapperLite from '../../kt-wrapper-lite/kt-wrapper-lite';
+import Divider from '../../kt-divider/divider';
 
 const SupplierRfxDashboard = () => (
 	<MainContent
@@ -24,13 +25,35 @@ const SupplierRfxDashboard = () => (
 				className="bold kt-sucess kt-transparent kt-primary tiny"
 			/>
 		</div>
-		<Divider classes="p-b-4" />
+		<Divider classes="p-b-4" type="faint" />
+		<div className="m-t-20 text-rights kt-primary">
+			This Bid is due in 17:40:33
+		</div>
 		<KtWrapperLite
 			classes="m-t-20"
 		>
 			<div className="">
-				<p>show deatils of the bid here</p>
+				<p>RSVP Deadline:</p>
+				<p>Question Deadline</p>
 			</div>
+		</KtWrapperLite>
+		<KtWrapperLite
+			classes="m-t-20"
+		>
+			<Divider type="thick" title="Description" />
+			<div className="m-t-20">
+				<p>lorem: Write all the descriptions of the bid here </p>
+			</div>
+		</KtWrapperLite>
+		<KtWrapperLite
+			classes="m-t-20"
+		>
+			<Divider type="thick" title="Rfp Questions" />
+		</KtWrapperLite>
+		<KtWrapperLite
+			classes="m-t-20"
+		>
+			<Divider type="thick" title="Rfp Attachments" />
 		</KtWrapperLite>
 		<div className="m-t-20">
 			<Link to="/supplier/events/:id/response">
