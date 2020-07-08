177e0804064350beb17dd36d941feadd4be486db
ebenKB <eakbo23@gmail.com>
2020-07-08 12:29:02 +0000

    refactor ui reducer

--- src/redux/reducers/ui.js
+++ src/redux/reducers/ui.js
@@ -36,97 +36,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     // buyer activities
-    case LOADING_RECENT_ACTIVITIES: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingRecentActivities: true,
-        },
-      };
-    }
-
-    case RECENT_ACTIVITIES_DONE_LOADING: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingRecentActivities: false,
-        },
-      };
-    }
-
-    case LOADING_BIDS: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingBids: true,
-        },
-      };
-    }
-
-    case BIDS_DONE_LOADING: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingBids: false,
-        },
-      };
-    }
-
-    case LOADING_ANALYTICS: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingAnalytics: true,
-        },
-
-      };
-    }
-
-    case ANALYTICS_DONE_LOADING: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingAnalytics: false,
-        },
-      };
-    }
-
-    case LOADING_SUPPLIER_RFP_CLOSING_SOON: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingSupplierRfpClosing: true,
-        },
-      };
-    }
-
-    case SUPPLIER_RFP_CLOSING_DONE_LOADING: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingSupplierRfpClosing: false,
-        },
-      };
-    }
-
-    case LOADING_RSVP_CLOSING_SOON: {
-      return {
-        ...state,
-        supplier: {
-          ...state.supplier,
-          isLoadingRSVPClosing: false,
-        },
-      };
-    }
-
     case IS_PUBLISHING_RFP: {
       return {
         ...state,
@@ -180,8 +89,99 @@ export default (state = initialState, action) => {
         isFetchingRfpStakeholder: false,
       };
     }
-
+    // end buyer activites
     // supplier activities
+    case LOADING_BIDS: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingBids: true,
+        },
+      };
+    }
+
+    case BIDS_DONE_LOADING: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingBids: false,
+        },
+      };
+    }
+
+    case LOADING_ANALYTICS: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingAnalytics: true,
+        },
+
+      };
+    }
+
+    case ANALYTICS_DONE_LOADING: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingAnalytics: false,
+        },
+      };
+    }
+
+
+    case LOADING_RSVP_CLOSING_SOON: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingRSVPClosing: false,
+        },
+      };
+    }
+    case LOADING_RECENT_ACTIVITIES: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingRecentActivities: true,
+        },
+      };
+    }
+
+    case RECENT_ACTIVITIES_DONE_LOADING: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingRecentActivities: false,
+        },
+      };
+    }
+
+    case LOADING_SUPPLIER_RFP_CLOSING_SOON: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingSupplierRfpClosing: true,
+        },
+      };
+    }
+
+    case SUPPLIER_RFP_CLOSING_DONE_LOADING: {
+      return {
+        ...state,
+        supplier: {
+          ...state.supplier,
+          isLoadingSupplierRfpClosing: false,
+        },
+      };
+    }
+    // end supplier activities
     default: {
       return { ...state };
     }
