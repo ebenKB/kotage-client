fdf18b65290874029a019ee4847d563cfed841c3
ebenKB <eakbo23@gmail.com>
2020-07-22 22:02:46 +0000

    enforce validation in new message components fix css issue in floating supplier component

--- src/components/message-center/new-message/new-message.jsx
+++ src/components/message-center/new-message/new-message.jsx
@@ -8,12 +8,13 @@ import { useHistory } from 'react-router-dom';
 import {
   Button, Checkbox, Icon, Label,
 } from 'semantic-ui-react';
+import { ValidatorForm } from 'react-form-validator-core';
 import { uploadFiles } from '../../../utils/app';
 import MainContent from '../../kt-main-content/mainContent';
 import KtTextArea from '../../form-fields/textarea/textarea';
 import KtWrapper from '../../kt-wrapper/kt-wrapper';
 import Help from '../../../utils/requisitions/new/help';
-import Input from '../../form-fields/input/input';
+// import Input from '../../form-fields/input/input';
 import { createRfpMessage } from '../../../redux/actions/rfpActions';
 import Dropzone from '../../dropzone/dropzone';
 import Divider from '../../kt-divider/divider';
@@ -21,7 +22,7 @@ import { RFP_MESSAGE_FOLDERNAME } from '../../../utils/app/definitions';
 import FloatingSupplierList from '../../floating-supplier-list/floating-supplier-list';
 import { setNotification } from '../../../redux/actions/appActions';
 import Can from '../../can/can';
-
+import InputValidator from '../../form-fields/input-validator/input-validator';
 
 const NewMessage = ({
   createNewMessage,
@@ -46,6 +47,7 @@ const NewMessage = ({
   const [canShowSuppliers, setCanShowSuppliers] = useState(false);
   const [selectedSuppliers, setSelectedSuppliers] = useState(null);
   const [hasSetAllSuppliers, setAllSuppliers] = useState(false);
+  const ref = React.createRef();
 
   const goBack = () => {
     if (history) {
@@ -102,20 +104,26 @@ const NewMessage = ({
   };
 
   const handleSubmit = async () => {
-    if (selectedSuppliers && selectedSuppliers.length > 0) {
-      const files = await
-      uploadFiles(message.files, tenantUid, RFP_MESSAGE_FOLDERNAME);
-      message.files = files;
-      setMessage(message);
-      let supplier_ids = null;
-      supplier_ids = selectedSuppliers.map((s) => s.account_id);
-      createNewMessage(message, supplier_ids)
-        .then(() => history.goBack());
-    } else {
-      showNotification({
-        message: 'Select at least one supplier to continue.',
-      }, 'error');
-    }
+    console.log('We want to submit the message', message);
+    const validatorForm = ref.current;
+    validatorForm.validate();
+    const isValid = await validatorForm.isFormValid();
+    if (isValid) {
+      if (selectedSuppliers && selectedSuppliers.length > 0) {
+        const files = await
+        uploadFiles(message.files, tenantUid, RFP_MESSAGE_FOLDERNAME);
+        message.files = files;
+        setMessage(message);
+        let supplier_ids = null;
+        supplier_ids = selectedSuppliers.map((s) => s.account_id);
+        createNewMessage(message, supplier_ids)
+          .then(() => history.goBack());
+      } else {
+        showNotification({
+          message: 'Select at least one supplier to continue.',
+        }, 'error');
+      }
+    } else console.log('The form is not valid');
   };
 
   return (
@@ -126,7 +134,6 @@ const NewMessage = ({
 		<KtWrapper
 			header="New message"
 		>
-			{/* <p>Messages you send about this RFP will be sent to all your suppliers.</p> */}
 			{currentProposal && (
 				<FloatingSupplierList
 					suppliers={currentProposal.suppliers}
@@ -135,76 +142,85 @@ const NewMessage = ({
 					handleAction={(suppliers) => handleAddSuppliers(suppliers)}
 				/>
 			)}
-			<div>
-				<Input
-					className="fluid m-b-10"
-					type="text"
-					placeholder="Subject"
-					onChange={(e) => handleInputChange(e)}
-					name="subject"
-					value={message.subject}
-				/>
-			</div>
-			<Can
-				perform="supplier:send_message"
-				accountType={accountType}
-				roleType={currentUser.is_admin ? 'admin' : 'user'}
-				yes={() => (
-					<div className="m-t-20 m-b-20">
-						<Checkbox
-							onChange={(e, data) => handleSetAllSuppliers(data)}
-							checked={hasSetAllSuppliers}
-							label="Send to all suppliers"
-						/>
-						<span>&nbsp;or&nbsp;</span>
-						<Button
-							className="kt-transparent kt-primary"
-							content="open supplier Directory"
-							onClick={showSuplierDirectory}
-						/>
-						<span>&nbsp;</span>
-						<span>to add suppliers</span>
-						<div className="m-t-20 flex-center">
-							{selectedSuppliers && selectedSuppliers.map((s) => (
-								<Label>
-									<Button
-										className="kt-transparent tiny m-r-5"
-										content={<Icon name="close" />}
-										onClick={() => removeSupplierFromReceipients(s)}
-									/>
-									{selectedSuppliers && s.company_name}
-								</Label>
-							))}
+			<ValidatorForm
+				ref={ref}
+				onSubmit={handleSubmit}
+			>
+				<div>
+					<InputValidator
+						className="fluid m-b-10"
+						type="text"
+						placeholder="Subject"
+						onChange={(e) => handleInputChange(e)}
+						name="subject"
+						value={message.subject}
+						instantValidate={false}
+						validators={['required', 'isString', 'minStringLength:8']}
+						errorMessages={['Subject is required', 'Subject is not valid', 'Subject is too short.']}
+					/>
+				</div>
+				<Can
+					perform="supplier:send_message"
+					accountType={accountType}
+					roleType={currentUser.is_admin ? 'admin' : 'user'}
+					yes={() => (
+						<div className="m-t-20 m-b-20">
+							<Checkbox
+								onChange={(e, data) => handleSetAllSuppliers(data)}
+								checked={hasSetAllSuppliers}
+								label="Send to all suppliers"
+							/>
+							<span>&nbsp;or&nbsp;</span>
+							<Button
+								className="kt-transparent kt-primary"
+								content="open supplier Directory"
+								onClick={showSuplierDirectory}
+							/>
+							<span>&nbsp;</span>
+							<span>to add suppliers</span>
+							<div className="m-t-20 flex-center wrap">
+								{selectedSuppliers && selectedSuppliers.map((s) => (
+									<Label>
+										<Button
+											className="kt-transparent tiny m-r-5"
+											content={<Icon name="close" />}
+											onClick={() => removeSupplierFromReceipients(s)}
+										/>
+										{selectedSuppliers && s.company_name}
+									</Label>
+								))}
+							</div>
 						</div>
-					</div>
-				)}
-				no={() => null}
-			/>
-			<KtTextArea
-				placeholder="Message"
-				rows={10}
-				classes="message-box fluid kt-bg-shadow"
-				onChange={handleTextChange}
-				value={message.message}
-			/>
-			<Divider type="faint" title="Attachment" classes="m-b-20 m-t-10" />
-			<Dropzone
-				onFilesChange={(files) => setFiles(files)}
-			/>
-			<div className="text-right m-t-20">
-				<Button
-					default
-					content="Go Back"
-					onClick={goBack}
+					)}
+					no={() => null}
 				/>
-				<Button
-					small
-					content="Send"
-					color="green"
-					onClick={handleSubmit}
-					loading={isLoading}
+				<KtTextArea
+					placeholder="Message"
+					rows={10}
+					classes="message-box fluid kt-bg-shadow"
+					onChange={handleTextChange}
+					value={message.message}
 				/>
-			</div>
+				<Divider type="faint" title="Attachment" classes="m-b-20 m-t-10" />
+				<Dropzone
+					onFilesChange={(files) => setFiles(files)}
+				/>
+				<div className="text-right m-t-20">
+					<Button
+						default
+						content="Go Back"
+						onClick={goBack}
+					/>
+					<Button
+						as="submit"
+						small
+						content="Send"
+						color="green"
+						onClick={handleSubmit}
+						loading={isLoading}
+					/>
+				</div>
+			</ValidatorForm>
 		</KtWrapper>
 	</MainContent>
   );
